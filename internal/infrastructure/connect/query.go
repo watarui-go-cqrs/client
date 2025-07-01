8326e8300c57f99d3a168364cf10e09712f08404
Wataru Ishikawa <wataruby@gmail.com>
2025-07-02 01:09:13 +0900

    feat: gRPC接続のTLS認証を追加し、セキュリティを強化しました

--- internal/infrastructure/connect/query.go
+++ internal/infrastructure/connect/query.go
@@ -1,8 +1,12 @@
 package connect
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"os"
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 type queryConnector struct{}
@@ -12,14 +16,27 @@ func NewQueryConnector() ServiceConnector {
 }
 
 func (c *queryConnector) Connect() (*grpc.ClientConn, error) {
-	server := "query-service:8083"
-	conn, err := grpc.Dial(
-		server,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithBlock(),
-	)
+	certFile := "certs/queryservice.pem"
+
+	pemData, err := os.ReadFile(certFile)
 	if err != nil {
 		return nil, err
 	}
-	return conn, nil
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(pemData) {
+		return nil, err
+	}
+	config := &tls.Config{
+		RootCAs: cp,
+	}
+	creds := credentials.NewTLS(config)
+	server := "query-service:8083"
+	if conn, err := grpc.Dial(
+		server,
+		grpc.WithTransportCredentials(creds),
+	); err != nil {
+		return nil, err
+	} else {
+		return conn, nil
+	}
 }
